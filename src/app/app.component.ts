e3a25613dd742c24becf68120671a1b8ea610d69
velrino <denismagalhaesvelrino@gmail.com>
2020-04-07 13:06:30 -0300

    :art: added object data, function getYear and calculate year old

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portifolio';
+export class AppComponent implements OnInit {
+  data = {
+    age: 21,
+    text: 'Lorem ipsum',
+    location: {
+      country: "Brasil",
+      state: "São Paulo",
+    }
+  }
+
+  ngOnInit() {
+    this.calculateYearOld();
+  }
+
+  getYear() {
+    return new Date().getFullYear();
+  }
+
+  calculateYearOld() {
+    const dates: any = {
+      born: new Date(),
+      today: new Date('6/10/1996'),
+    }
+    const daysInYear = 365;
+
+    const diffTime = Math.abs(dates.today - dates.born);
+    const diffDays = Math.ceil((diffTime / (1000 * 60 * 60 * 24))/daysInYear);
+
+    return diffDays-1;
+  }
 }
